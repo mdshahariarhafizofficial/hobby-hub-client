091d34336b12061cc83a8a9b64cdaff833750c50
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-05-22 16:08:53 +0600

    Update Group Form Design dome

--- src/Pages/MyGroups.jsx
+++ src/Pages/MyGroups.jsx
@@ -3,13 +3,16 @@ import { Link, useLoaderData } from 'react-router';
 import { AuthContext } from '../Context/AuthContext';
 import GroupCard from '../Components/GroupCard/GroupCard';
 import { FaEdit, FaInfoCircle } from 'react-icons/fa';
-import { MdDeleteForever } from 'react-icons/md';
+import { MdDeleteForever, MdGroupAdd } from 'react-icons/md';
 import Swal from 'sweetalert2';
+import { IoSave } from 'react-icons/io5';
 
 const MyGroups = () => {
     const {user} = useContext(AuthContext);
     const data = useLoaderData();
     const [myGroups, setMyGroups] = useState(data);
+    const [singleGroup, setSingleGroup] = useState(myGroups);
+    
 
     useEffect(()=>{
         const remainingGroups = data.filter(group => group.email === user.email );
@@ -48,6 +51,25 @@ const MyGroups = () => {
         });
         
     }
+
+    // Get Single Group Data 
+    const handleUpdateData = (id) => {
+        const remainingGroups = data.find(group => group._id === id );
+        console.log(remainingGroups);
+        setSingleGroup(remainingGroups) 
+    }
+
+    // Handle Update Data
+    const sendUpdateDataToDb = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const formData = new FormData(form);
+        const updateData = Object.fromEntries(formData.entries());
+        console.log(updateData);
+        
+    }
+    
+    
     
     return (
         <div>
@@ -88,7 +110,11 @@ const MyGroups = () => {
                                             <FaInfoCircle size={22} color='#ff5722'></FaInfoCircle>
                                         </button>
                                     </Link>
-                                    <button className=" join-item">
+                                    {/* Edit */}
+                                    <button onClick={()=>{
+                                        document.getElementById('my_modal_5').showModal()
+                                        handleUpdateData(group._id)
+                                        }} className=" join-item">
                                         <FaEdit 
                                         size={22} color='#1e0a3c'></FaEdit>
                                     </button>
@@ -107,6 +133,182 @@ const MyGroups = () => {
                 </table>
                 </div>
             </div>
+
+            {/* Modal */}
+            {/* Open the modal using document.getElementById('ID').showModal() method */}
+            <dialog id="my_modal_5" className="modal">
+            <div className="modal-box w-11/12 max-w-5xl">
+
+                {/* Content */}
+                <div className='my-6'>
+                    <h2 className='text-5xl font-bold text-center text-secondary'>Update Group</h2>
+                    <div className="divider divider-primary w-60 mx-auto"></div>
+                </div>
+
+                {/* Form  Section*/}
+                <div className=''>
+                        <div className="card w-full max-w-full shrink-0">
+
+                            {/* Form */}
+                            <form onSubmit={sendUpdateDataToDb} className="card-body p-0">
+                                <fieldset className="fieldset">
+                                    
+
+                                    <div className='grid md:grid-cols-2 gap-5'>
+                                        <div>
+                                            {/* User Name */}
+                                            <label className="label text-secondary font-bold">User Name</label>
+                                            <input 
+                                            type="text"
+                                            name='userName' 
+                                            className="input w-full"
+                                            defaultValue={singleGroup?.userName}
+                                            readOnly
+                                            placeholder="User Name" />
+                                            {/* ------------ */}                                           
+                                        </div>
+                                        <div>
+                                            {/* User Email */}
+                                            <label className="label text-secondary font-bold">User Email</label>
+                                            <input 
+                                            type="email"
+                                            name='email' 
+                                            className="input w-full" 
+                                            defaultValue={singleGroup?.email}
+                                            readOnly
+                                            placeholder={`${user? user.email: "User Email"}`} />
+                                            {/* ------------ */} 
+                                        </div>
+                                    </div>
+
+
+                                    <div className='grid md:grid-cols-2 gap-5'>
+                                        <div>
+                                            {/* Group Name */}
+                                            <label className="label text-secondary font-bold">Group Name</label>
+                                            <input 
+                                            type="text"
+                                            name='groupName' 
+                                            className="input w-full" 
+                                            defaultValue={singleGroup?.groupName}
+                                            placeholder="Group Name"
+                                            required />
+                                            {/* ------------ */}
+                                        </div>
+
+                                        <div>
+                                            {/* Hobby Category Name */}
+                                            <label className="label text-secondary font-bold">Hobby Category</label>
+                                            <select name='category' className='w-full border border-gray-300 py-3 px-2 text-primary font-bold rounded' required>
+
+                                                <option value={`${singleGroup?.category}`}>
+                                                    {singleGroup?.category}
+                                                </option>
+
+                                                <option
+                                                className='text-secondary font-bold'
+                                                value="drawing-and-painting">Drawing & Painting</option>
+                                                <option
+                                                className='text-secondary font-bold'
+                                                value="photography">Photography</option>
+                                                <option
+                                                className='text-secondary font-bold'
+                                                value="video-gaming">Video Gaming</option>
+                                                <option
+                                                className='text-secondary font-bold'
+                                                value="fishing">Fishing</option>
+                                                <option
+                                                className='text-secondary font-bold'
+                                                value="running">Running</option>
+                                                <option
+                                                className='text-secondary font-bold'
+                                                value="cooking">Cooking</option>
+                                                <option
+                                                className='text-secondary font-bold'
+                                                value="reading">Reading</option>
+
+                                            </select>
+                                        </div>
+                                    </div>
+
+
+                                    <div className='grid md:grid-cols-2 gap-5'>
+                                        <div>
+                                            {/* Start Date */}
+                                            <label className="label text-secondary font-bold">Start Date</label>
+                                            <input 
+                                            type="date"
+                                            name='date'
+                                            defaultValue={singleGroup?.date || ''}
+                                            className="input w-full" 
+                                            placeholder="Start Date"
+                                            required />
+                                            {/* ------------ */}
+                                        </div>
+                                        <div>
+                                            {/* Max Members */}
+                                            <label className="label text-secondary font-bold">Max Members</label>
+                                            <input 
+                                            type="number"
+                                            name='maxMembers'
+                                            defaultValue={singleGroup?.maxMembers}
+                                            className="input w-full" 
+                                            placeholder="Max Members"
+                                            required />
+                                            {/* ------------ */}
+                                        </div>
+                                    </div>
+
+
+                                    {/* Meeting Location */}
+                                    <label className="label text-secondary font-bold">Meeting Location</label>
+                                    <input 
+                                    type="text"
+                                    name='meetingLocation'
+                                    defaultValue={singleGroup?.meetingLocation} 
+                                    className="input w-full" 
+                                    placeholder="Meeting Location"
+                                    required />
+                                    {/* ------------ */}
+
+                                    {/* Image URL */}
+                                    <label className="label text-secondary font-bold">Image URL</label>
+                                    <input 
+                                    type="url"
+                                    name='imageUrl'
+                                    defaultValue={singleGroup?.imageUrl} 
+                                    className="input w-full" 
+                                    placeholder="Image URL"
+                                    required />
+                                    {/* ------------ */}
+
+                                    {/* Description */}
+                                    <label className="label text-secondary font-bold">Description</label>
+                                    <textarea 
+                                    placeholder="Description"
+                                    name='description'
+                                    defaultValue={singleGroup?.description}
+                                    required
+                                    className="textarea textarea-lg w-full"></textarea>
+                                    {/* ------------ */}
+                                    
+                                    <button 
+                                    type='submit' className="btn btn-primary mt-4 text-xl text-secondary font-bold">
+                                        <IoSave size={25}></IoSave>Update Group</button>
+                                </fieldset>
+                            </form>
+                            </div>
+                </div>
+
+
+                <div className="modal-action">
+                <form method="dialog">
+                    {/* if there is a button in form, it will close the modal */}
+                     <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2 bg-primary text-white">✕</button>
+                </form>
+                </div>
+            </div>
+            </dialog>
 
         </div>
     );
